61ad0be5b7e3eeec0487932b89aa163e65b786c7
Meia Alsup <malsup@google.com>
2020-10-22 00:11:06 +0000

    fix tldr installation

--- linux.sh
+++ linux.sh
@@ -1,6 +1,5 @@
 #!/bin/zsh
 
-sudo apt install tldr
 sudo apt install htop
 sudo apt install tmux
 sudo apt install wget
@@ -8,3 +7,8 @@ sudo apt install fzf
 sudo apt install wdiff
 sudo apt install zsh-autosuggestions
 sudo apt install shellcheck
+
+# install tldr
+mkdir -p ~/bin
+curl -o ~/bin/tldr https://raw.githubusercontent.com/raylee/tldr/master/tldr
+chmod +x ~/bin/tldr
