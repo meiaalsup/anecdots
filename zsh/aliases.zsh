c051bea3828547ea9e96bbec369075c9b97909af
Meia Alsup <malsup@google.com>
2022-10-05 16:50:30 -0500

    more autoformat shumai path stuff

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,5 +1,6 @@
 alias sz="source ~/.zshrc"
 alias reloadshell="source ~/.zshrc"
+alias shumai="cd ~/dev/shumai"
 
 # Skip forward/back a word with opt-arrow at command line
 bindkey '[C' forward-word
