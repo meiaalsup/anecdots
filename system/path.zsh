9662eeb1a28731343e6b3fdcdce7d68536d0ba05
meiaalsup <meia.alsup@gmail.com>
2024-01-30 15:50:04 -0500

    clean up system path generation

--- system/path.zsh
+++ system/path.zsh
@@ -1,1 +1,16 @@
-export PATH="/usr/local/opt:/usr/local/bin:/usr/local/sbin:$HOME/bin:$HOME/.local/bin:$PATH"
+# Define the list of directories you wish to add to PATH
+directories=(
+  /usr/local/opt
+  /usr/local/bin
+  /usr/local/sbin
+  $HOME/bin
+  $HOME/.local/bin
+  $(brew --prefix)/opt/python@3/libexec/bin
+)
+
+# Add them to the PATH
+for dir in "${directories[@]}" ; do
+  PATH="$dir:$PATH"
+done
+
+export PATH
