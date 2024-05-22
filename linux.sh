e3bbe8670ea3ea8afdbffa0df04b6203d158075e
meiaalsup <meia.alsup@gmail.com>
2024-05-22 15:04:26 -0400

    modify linux script to work on amazon ec2 by default

--- linux.sh
+++ linux.sh
@@ -3,19 +3,18 @@
 sudo yum install htop
 sudo yum install tmux
 sudo yum install wget
-#sudo yum install fzf
 
 #sudo yum install wdiff
 #sudo yum install zsh-autosuggestions
 #sudo yum install shellcheck
 
 # install fzf
-if [ ! -d ~/.fzf ]; then
+
+if [ ! -d "$HOME/.fzf" ]; then
     git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-    ~/.fzf/install
+        ~/.fzf/install
+    else
+        echo "fzf is already installed"
 fi
 
-# install tldr
-mkdir -p ~/bin
-curl -o ~/bin/tldr https://raw.githubusercontent.com/raylee/tldr/master/tldr
-chmod +x ~/bin/tldr
+#sudo yum install -y ShellCheck
